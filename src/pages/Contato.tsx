ad78c1cb5239a8843ef193998de40d8186807db4
Carla Sama <carlavxsamaniego@gmail.com>
2025-04-02 16:29:36 -0400

    Updates

--- src/pages/Contato.tsx
+++
@@ -1,6 +0,0 @@
-const Contato: React.FC = () => {
-    return <h1>Página de Projetos</h1>;
-  };
-  
-  export default Contato;
-  
--- src/pages/Projetos.tsx
+++
@@ -1,6 +0,0 @@
-const Projetos: React.FC = () => {
-    return <h1>Página de Projetos</h1>;
-  };
-  
-  export default Projetos;
-  
